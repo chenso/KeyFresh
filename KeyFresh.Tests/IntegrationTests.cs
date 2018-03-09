4b7336c014387983ca38bd989562f129b23ccbcf
Songge Chen <songge.chen@careotter.onmicrosoft.com>
2018-03-09 15:00:30 -0600

    Integration test tests concurrency

--- KeyFresh.Tests/IntegrationTests.cs
+++ KeyFresh.Tests/IntegrationTests.cs
@@ -3,18 +3,25 @@ using Xunit;
 using KeyFresh;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace KeyFresh.UnitTests
 {
     public class IntegrationTests
     {
-        [Fact(Skip = "Define own keyvault")]
+        [Fact(Skip = "Implement KeyVaultMock with own credentials")]
         public async void RefreshClient_ExecuteAsync_Retry_Success()
         {
             var refreshKey = new RefreshKey(new Uri("https://google.com"), new KeyVaultMock());
             var blobClientProvider = new BlobClientProvider(refreshKey);
             var refreshClient = new CloudBlobRefreshClient(blobClientProvider);
-            var result = await refreshClient.ExcecuteAsync(x => x.ListContainersSegmentedAsync(null));
+            var tasks = new List<Task<ContainerResultSegment>>();
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(refreshClient.ExcecuteAsync(x => x.ListContainersSegmentedAsync(null)));
+            }
+
+            var results = await Task.WhenAll(tasks).ConfigureAwait(false);
         }
     }
 }
